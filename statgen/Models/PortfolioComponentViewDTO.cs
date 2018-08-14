ce665e5c86dfd798e6a17477822643f8f5d280fd
Paulo Chang <pigenio@gmail.com>
2018-08-14 17:03:01 +0200

    Added DTO to short portfolio status in HTML table.

---
+++ statgen/Models/PortfolioComponentViewDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace statgen.Models
+{
+    public class PortfolioComponentViewDTO
+    {
+        public PortfolioComponentViewDTO(int stockId, int qty, string stockName, double unitValue, double totalValue)
+        {
+            StockId = stockId;
+            Qty = qty;
+            StockName = stockName;
+            UnitValue = unitValue;
+            TotalValue = totalValue;
+        }
+
+        public int StockId { get; }
+
+        public int Qty { get; }
+
+        public string StockName { get; }
+
+        public double UnitValue { get; }
+
+        public double TotalValue { get; }
+    }
+}
