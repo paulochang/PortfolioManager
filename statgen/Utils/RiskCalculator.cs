e2c682c978da1f035a3ac1e9b08278521ab5d6cd
Paulo Chang <pigenio@gmail.com>
2018-08-15 18:18:38 +0200

    Add Var and ES Risk calculator. - Add multithread var and es calculation

---
+++ statgen/Utils/RiskCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using statgen.Models;
+
+namespace statgen.Utils
+{
+    public static class RiskCalculator
+    {
+        public static async Task<RiskStatsViewDto> GetAssetRiskStats(List<double> fullReturnSet)
+        {
+            ImmutableList<double> roReturnSet = fullReturnSet.ToImmutableList();
+
+            (double var, double es) varAndEs90 = await Task.Run(() => GetVarAndEsAtConfidenceLvl(roReturnSet, 90));
+            (double var, double es) varAndEs95 = await Task.Run(() => GetVarAndEsAtConfidenceLvl(roReturnSet, 95));
+            (double var, double es) varAndEs99 = await Task.Run(() => GetVarAndEsAtConfidenceLvl(roReturnSet, 99));
+
+            RiskStatsViewDto partialResult = new RiskStatsViewDto(
+                varAndEs90.var, varAndEs90.es,
+                varAndEs95.var, varAndEs95.es,
+                varAndEs99.var, varAndEs99.es
+            );
+            return partialResult;
+        }
+
+        private static (double var, double es) GetVarAndEsAtConfidenceLvl(ImmutableList<double> returnsSet,
+            int confidenceLevel)
+        {
+            int fullCount = returnsSet.Count;
+            int confidenceLvlCount = fullCount * (100 - confidenceLevel) / 100;
+
+            ImmutableList<double> confidenceLvlReturnsList = returnsSet.Take(confidenceLvlCount).ToImmutableList();
+
+            double var = confidenceLvlReturnsList.Last();
+            double es = confidenceLvlReturnsList.AsParallel().Average();
+
+            return (var, es);
+        }
+    }
+}
