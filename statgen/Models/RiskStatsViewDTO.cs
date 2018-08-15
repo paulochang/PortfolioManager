1a20e74189149b40c81afdf24ad1b97b5146f086
Paulo Chang <pigenio@gmail.com>
2018-08-15 18:20:30 +0200

    Create Portfolio and Stock Risk Stats DTO - Create base RiskStatsViewDTO to model general VAR and ES at 90/95/99 confidence level - Create child Portfolio and Risk DTO with additional info

---
+++ statgen/Models/RiskStatsViewDTO.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace statgen.Models
+{
+    public class RiskStatsViewDto
+    {
+        public RiskStatsViewDto(double valueAtRisk90, double expectedShortfall90, double valueAtRisk95, double expectedShortfall95, double valueAtRisk99, double expectedShortfall99)
+        {
+            ValueAtRisk90 = valueAtRisk90;
+            ExpectedShortfall90 = expectedShortfall90;
+            ValueAtRisk95 = valueAtRisk95;
+            ExpectedShortfall95 = expectedShortfall95;
+            ValueAtRisk99 = valueAtRisk99;
+            ExpectedShortfall99 = expectedShortfall99;
+        }
+
+        // 90-confidence level
+        public double ValueAtRisk90 { get;  }
+        public double ExpectedShortfall90 { get; }
+        
+        // 95-confidence level
+        public double ValueAtRisk95 { get;  }
+        public double ExpectedShortfall95 { get;  }
+        
+        // 99-confidence level
+        public double ValueAtRisk99 { get; }
+        public double ExpectedShortfall99 { get;}
+    }
+    
+    public class PortfolioRiskStatsViewDto : RiskStatsViewDto
+    {
+        public PortfolioRiskStatsViewDto(
+            int portfolioId, 
+            double valueAtRisk90, double expectedShortfall90, 
+            double valueAtRisk95, double expectedShortfall95, 
+            double valueAtRisk99, double expectedShortfall99
+            ) : base(
+                valueAtRisk90, expectedShortfall90, 
+                valueAtRisk95, expectedShortfall95, 
+                valueAtRisk99, expectedShortfall99
+            )
+        {
+            PortfolioId = portfolioId;
+        }
+
+        public PortfolioRiskStatsViewDto(
+            int portfolioId, 
+            RiskStatsViewDto riskStatsViewDto
+            ) : base(
+            riskStatsViewDto.ValueAtRisk90, riskStatsViewDto.ExpectedShortfall90, 
+            riskStatsViewDto.ValueAtRisk95, riskStatsViewDto.ExpectedShortfall95, 
+            riskStatsViewDto.ValueAtRisk99, riskStatsViewDto.ExpectedShortfall99
+            )
+        {
+            PortfolioId = portfolioId;
+        }
+
+        public int PortfolioId { get; }
+    }
+    
+    public class StockRiskStatsViewDto : RiskStatsViewDto
+    {
+        public StockRiskStatsViewDto(
+            int stockId, string stockName, 
+            RiskStatsViewDto riskStatsViewDto
+            ) : base(
+            riskStatsViewDto.ValueAtRisk90, riskStatsViewDto.ExpectedShortfall90, 
+            riskStatsViewDto.ValueAtRisk95, riskStatsViewDto.ExpectedShortfall95, 
+            riskStatsViewDto.ValueAtRisk99, riskStatsViewDto.ExpectedShortfall99
+            )
+        {
+            StockId = stockId;
+            StockName = stockName;
+        }
+        
+        public StockRiskStatsViewDto(
+            int stockId, string stockName, 
+            double valueAtRisk90, double expectedShortfall90, 
+            double valueAtRisk95, double expectedShortfall95, 
+            double valueAtRisk99, double expectedShortfall99
+        ) : base(
+            valueAtRisk90, expectedShortfall90, 
+            valueAtRisk95, expectedShortfall95, 
+            valueAtRisk99, expectedShortfall99
+        )
+        {
+            StockId = stockId;
+            StockName = stockName;
+        }
+
+        public int StockId { get; }
+        public string StockName { get; }
+    }
+}
