d99d200680bbac0b608176dbd17fb7c735788f8d
Paulo Chang <pigenio@gmail.com>
2018-08-20 18:22:32 +0200

    Create Stock Price DTO to track past changes.

---
+++ statgen/Models/StockPriceDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace statgen.Models
+{
+    public class StockPriceDto
+    {
+        public DateTime DateTime { get; set; }
+        public Double Price { get; set; }
+    }
+}
